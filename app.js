f5950eaf2ad1b3118dd4a3897df5bebac700e5f7
Louiza Mimouni <zoopertrooper33@outlook.com>
2025-10-09 10:38:29 +0100

    feat: add getComputerChoice() and quick test

---
+++ app.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+  const n = Math.floor(Math.random() * 3); // 0,1,2
+  if (n === 0) return "rock";
+  if (n === 1) return "paper";
+  return "scissors";
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log("CPU:", getComputerChoice());
+}
