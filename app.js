06116e17735dc596afd5fe9faa8ba4fecc55b5b7
Louiza Mimouni <zoopertrooper33@outlook.com>
2025-10-09 10:45:27 +0100

    feat: add global score variables

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
+// quick check
+console.log({ humanScore, computerScore });
+
+
 function getComputerChoice() {
   const n = Math.floor(Math.random() * 3); // 0,1,2
   if (n === 0) return "rock";
